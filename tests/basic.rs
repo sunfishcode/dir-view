5b35830c3c5e4d4d08e9039d314bbeeca7b8c081
Dan Gohman <dev@sunfishcode.online>
2023-01-23 15:59:15 -0800

    Add fs_utf8 tests.

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,4 +1,6 @@
-use cap_tempfile::{TempDir, ambient_authority};
+use cap_tempfile::{ambient_authority, TempDir};
+#[cfg(feature = "fs_utf8")]
+use dir_view::DirViewUtf8;
 use dir_view::{DirView, ViewKind};
 
 #[test]
@@ -13,12 +15,97 @@ fn basic() {
     // A readonly view can't change anything.
     let dir = temp_dir.open_dir(".").unwrap();
     let readonly = DirView::from_dir(dir, ViewKind::Readonly);
-    assert_eq!(readonly.create("hello").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.create("create").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.create_dir("create_dir").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.rename("hello", &readonly, "to").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.remove_file("hello").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.remove_dir(".").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.symlink("hello", "symlink").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
-    assert_eq!(readonly.hard_link("hello", &readonly, "hard_link").unwrap_err().kind(), std::io::ErrorKind::PermissionDenied);
+    assert_eq!(
+        readonly.create("hello").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.create("create").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.create_dir("create_dir").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly
+            .rename("hello", &readonly, "to")
+            .unwrap_err()
+            .kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.remove_file("hello").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.remove_dir(".").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.symlink("hello", "symlink").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly
+            .hard_link("hello", &readonly, "hard_link")
+            .unwrap_err()
+            .kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+}
+
+#[cfg(feature = "fs_utf8")]
+#[test]
+fn basic_utf8() {
+    let temp_dir = TempDir::new(ambient_authority()).unwrap();
+
+    // A full view can do everything.
+    let dir = temp_dir.open_dir(".").unwrap();
+    let dir = cap_std::fs_utf8::Dir::from_cap_std(dir);
+    let full = DirViewUtf8::from_dir(dir, ViewKind::Full);
+    full.create("hello").unwrap();
+
+    // A readonly view can't change anything.
+    let dir = temp_dir.open_dir(".").unwrap();
+    let dir = cap_std::fs_utf8::Dir::from_cap_std(dir);
+    let readonly = DirViewUtf8::from_dir(dir, ViewKind::Readonly);
+    assert_eq!(
+        readonly.create("hello").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.create("create").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.create_dir("create_dir").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly
+            .rename("hello", &readonly, "to")
+            .unwrap_err()
+            .kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.remove_file("hello").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.remove_dir(".").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly.symlink("hello", "symlink").unwrap_err().kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        readonly
+            .hard_link("hello", &readonly, "hard_link")
+            .unwrap_err()
+            .kind(),
+        std::io::ErrorKind::PermissionDenied
+    );
 }
